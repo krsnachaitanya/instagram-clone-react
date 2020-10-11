e3d084831e216a248f821c4478a888c1bd141400
Chaitu <chaitu.ca539@gmail.com>
2020-10-12 00:53:57 +0530

    Post Component Styling

--- src/App.js
+++ src/App.js
@@ -1,13 +1,39 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
 import Header from "./components/header/header.component";
 import Post from "./components/post/post.component";
 
 function App() {
+  const [posts, setPosts] = useState([
+    {
+      username: "coderchaitu",
+      caption: "react instagram clone",
+      imageUrl:
+        "https://images.unsplash.com/photo-1461749280684-dccba630e2f6?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60",
+    },
+    {
+      username: "coderchaitu",
+      caption: "react instagram clone",
+      imageUrl:
+        "https://images.unsplash.com/photo-1461749280684-dccba630e2f6?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60",
+    },
+    {
+      username: "coderchaitu",
+      caption: "react instagram clone",
+      imageUrl:
+        "https://images.unsplash.com/photo-1461749280684-dccba630e2f6?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60",
+    },
+  ]);
   return (
     <div className="App">
       <Header />
-      <Post />
+      {posts.map((post) => (
+        <Post
+          username={post.username}
+          imageUrl={post.imageUrl}
+          caption={post.caption}
+        />
+      ))}
     </div>
   );
 }
--- src/components/post/post.component.jsx
+++ src/components/post/post.component.jsx
@@ -1,12 +1,25 @@
 import React from "react";
 import "./post.styles.scss";
+import { Avatar } from "@material-ui/core";
 
-function Post() {
+function Post({ username, caption, imageUrl }) {
   return (
-    <div>
-      <h3>Username</h3>
-      <img src="" alt="" />
-      <h4>Username: caption</h4>
+    <div className="post__container">
+      <div className="post__header">
+        <Avatar
+          className="post__avatar"
+          alt={username}
+          src="/static.images/avatar/1.jpg"
+        />
+        <h3>{username}</h3>
+      </div>
+      <img className="post__image" src={imageUrl} alt="" />
+      <div className="post__footer">
+        <p className="post__text">
+          <strong>{username}</strong>
+          <span>{caption}</span>
+        </p>
+      </div>
     </div>
   );
 }
